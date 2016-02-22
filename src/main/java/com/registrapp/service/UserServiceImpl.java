6cce7ee7a1125ad502e2ee6f959dd96b087075fd
Dima <d.gachok@gmail.com>
2016-02-22 16:59:08 +0200

    Redeploy

--- src/main/java/com/registrapp/service/UserServiceImpl.java
+++ src/main/java/com/registrapp/service/UserServiceImpl.java
@@ -5,7 +5,7 @@ import com.registrapp.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 
-import org.springframework.security.authentication.encoding.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,8 +22,8 @@ public class UserServiceImpl implements UserService {
         user.setSsoId(new Random(System.currentTimeMillis()).nextInt(1000000) + 10000);
         user.setUser_role_id(1);
         user.setAccount_status("disabled");
-        PasswordEncoder encoder = new Md5PasswordEncoder();
-        String hashedPass = encoder.encodePassword(user.getPassword(), user);
+        PasswordEncoder encoder = (PasswordEncoder) new Md5PasswordEncoder();
+        String hashedPass = encoder.encode(user.getPassword());
         user.setPassword(hashedPass);
         userDao.addUser(user);
     }
