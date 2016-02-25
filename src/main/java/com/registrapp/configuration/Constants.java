4e72ef4edb403675be453e743b50cca6a5f8a84b
Dima <d.gachok@gmail.com>
2016-02-25 19:26:23 +0200

    Redeploy

--- src/main/java/com/registrapp/configuration/Constants.java
+++ src/main/java/com/registrapp/configuration/Constants.java
@@ -12,5 +12,5 @@ public class Constants {
 
     public static final String FORGET = "/forget?id=";
 
-    public static final String WebRootPath = "/var/lib/openshift/56c9aa1e7628e104ec00001d/jbossews/upload/";
+    public static final String WebRootPath = "/upload/";
 }
